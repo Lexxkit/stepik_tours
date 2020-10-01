91b1296a6f885034023c08ab58b918fef1800d48
Lexxkit <lexxkit@gmail.com>
2020-10-01 16:33:52 +0300

    Add custom handlers for 404, 500.

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+k!o^^s9j*fu_&fk)cgcjsis64$bu9-195q56isdr0gu)s3#*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
--- tours/views.py
+++ tours/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound, HttpResponseServerError
 
 from django.views import View
 
@@ -11,13 +11,23 @@ class MainView(View):
 
 class DepartureView(View):
     def get(self, request, departure, *args, **kwargs):
-        if not departure:
+        # raise 404 if url parameter is incorrect
+        if departure not in ['msk', 'spb', 'nsk', 'ekb', 'kzn']:
             raise Http404
         return render(request, 'tours/departure.html')
 
 
 class TourView(View):
     def get(self, request, id, *args, **kwargs):
-        if not id:
+        # raise 404 if url parameter is incorrect
+        if id not in range(1, 11):
             raise Http404
         return render(request, 'tours/tour.html')
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ой, что то сломалось... Простите извините!')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Кажется возникли проблемы...')
